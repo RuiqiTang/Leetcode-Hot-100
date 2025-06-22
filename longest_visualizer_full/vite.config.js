3447fc490e4b38d25975eff8b10ba86007017abe
Ruiqi Tang <RuiQiTarng@gmail.com>
2025-06-22 19:23:26 +0800

    Update vite.config.js

--- longest_visualizer_full/vite.config.js
+++ longest_visualizer_full/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:'/Leetcode-Hot-100/longest_visualizer_full/',
-  plugins: [react()],
-});
+  base: '/Leetcode-Hot-100/longest_visualizer_full/',
+  plugins: [react()]
+})
